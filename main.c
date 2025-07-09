240c1e99a7076e7abb647ec39044d3c7369d9aa3
voicemails <170760592+voicemails@users.noreply.github.com>
2025-07-09 16:21:07 +0200

    Write helper functions for retrieving screen resolution

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 
 #define SCREEN_HANDLE NULL
 
+int GetScreenWidth() {
+    return GetSystemMetrics(SM_CXSCREEN);
+}
+
+int GetScreenHeight() {
+    return GetSystemMetrics(SM_CYSCREEN);
+}
+
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int showCommand) {
     HDC screenDeviceContext = GetDC(SCREEN_HANDLE);
     {
-        int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-        int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-        printf("Screen Resolution: %dx%d\n", screenWidth, screenHeight);
+        printf("Screen Resolution: %dx%d\n", GetScreenWidth(), GetScreenHeight());
     }
     ReleaseDC(SCREEN_HANDLE, screenDeviceContext);
 
