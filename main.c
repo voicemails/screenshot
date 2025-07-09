e90cc9e1088c7dbfcb4680e849f121f239451a95
voicemails <170760592+voicemails@users.noreply.github.com>
2025-07-09 15:49:19 +0200

    Get resolution of the screen measured in pixels

--- main.c
+++ main.c
@@ -5,7 +5,11 @@
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int showCommand) {
     HDC screenDeviceContext = GetDC(SCREEN_HANDLE);
-
+    {
+        int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+        int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+        printf("Screen Resolution: %dx%d\n", screenWidth, screenHeight);
+    }
     ReleaseDC(SCREEN_HANDLE, screenDeviceContext);
 
     return 0;
